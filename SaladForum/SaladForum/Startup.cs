1658afc092020736647829e6b7316e9e865c961b
DESKTOP-RC09QPR\Awsome <sayuri.programmer.girl@gmail.com>
2017-06-05 19:03:33 +0300

    Initial Commit

---
+++ SaladForum/SaladForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaladForum.Startup))]
+namespace SaladForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
